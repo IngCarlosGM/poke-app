055b227ee19dd774710b678a1f397c992ba76653
Carlos Gamboa <gamboamc@outlook.com>
2023-03-19 17:48:12 -0500

    :bug: Adjusting non-icon styles and correcting input data types

--- src/components/atoms/Input/_InputStyled.tsx
+++ src/components/atoms/Input/_InputStyled.tsx
@@ -2,7 +2,11 @@ import tw, { styled } from 'twin.macro';
 
 export const _InputStyled = styled.input`
   ${tw`text-fourth text-sm rounded-md h-8 p-1 pr-10 pl-2 outline-cyan-500 border border-solid border-2 invalid:border-red-500`}
-  ${({ isSubmit }: { isSubmit: boolean }) =>
-    isSubmit &&
-    tw`bg-fourth text-white w-40 h-10 text-center hover:bg-opacity-70 pr-3 cursor-pointer`}
+  ${({ isSubmit, hasIcon }: { isSubmit: boolean; hasIcon: boolean }) => {
+    return [
+      isSubmit &&
+        tw`bg-fourth text-white w-40 h-10 text-center hover:bg-opacity-70 pr-3 cursor-pointer`,
+      !hasIcon && tw`pr-2`,
+    ];
+  }}
 `;
